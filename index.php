bd659be77091d56351a02ae79504290d9dc0ecec
mohsen-setoudeh <mohsen.setoudeh@tutanota.com>
2022-04-01 17:16:27 +0430

    Fixed </h3> and test somthing

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ $html_page=<<< ENDHTML
 		</form>
 
 		<p>This is the first file in my new Git Repo</p>
-		<h3>The new branch/h3>
+		<h3>Test of pull request</h3>
 
 		<p><em>This is small php script</em></p>
 
