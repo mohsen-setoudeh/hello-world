4a9eeae50163de2bf085b12d25e469d2fdbd59d8
mohsen-setoudeh <mohsen.setoudeh@tutanota.com>
2022-04-01 11:45:38 +0430

    merged with image after editing

--- index.php
+++ index.php
@@ -17,16 +17,16 @@ $html_page=<<< ENDHTML
 		<form action="info.php" method="POST">
 		<input type="submit" name="submit" value="info"/>
 		</form>
-<<<<<<< HEAD
+
 		<p>This is the first file in my new Git Repo</p>
 		<p>This line is here to show how merging works.</p>
-=======
+
 		<div>
 			<img src="lamp.png" alt="PHP logo">
 
 		</div>
 		
->>>>>>> images-add
+
 		<p><em>This is small php script</em></p>
 
 
